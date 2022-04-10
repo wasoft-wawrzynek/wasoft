fdad662b5ef729d91fd324f9fc45977f903e7064
Laurer <pawel.wawrzynek@gmail.com>
2022-04-10 18:19:34 +0200

    Cleaned up react dependencies

--- client-app/src/index.tsx
+++ client-app/src/index.tsx
@@ -1,16 +1,15 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const container = document.getElementById("root");
 if (!container) throw new Error("Failed to find the root element");
-const root = createRoot(container);
-
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  container
 );
 
 // If you want to start measuring performance in your app, pass a function
