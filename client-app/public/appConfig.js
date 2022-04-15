b074f9b73a5a86007b1c661458d07b4ea4dbe599
Laurer <pawel.wawrzynek@gmail.com>
2022-04-15 11:47:06 +0200

    Minor clarifications

--- client-app/public/appConfig.js
+++ client-app/public/appConfig.js
@@ -1,4 +1,4 @@
-// Debug values, replaced on start-up in production, by the env.sh script
+// Debug values, to be replaced on deployemtn (by the env.sh script for Docker, or by *.release.js file on Azure Web App)
 window['appConfig'] = {
     REACT_APP_API_URL: "http://localhost:7071/"
 }
--- client-app/public/appConfig.release.js
+++ client-app/public/appConfig.release.js
@@ -1,3 +1,4 @@
+// Values to be updated with release-time variables (for Azure App Service Web App release only)
 window['appConfig'] = {
-    REACT_APP_API_URL: "#{apiUrl}#"
+    REACT_APP_API_URL: "#{ClientApp.ApiUrl}#"
 }
