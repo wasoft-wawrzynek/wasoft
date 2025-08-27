41293ebb1e75fd06fe3a481a70f9ddc6f8a2386c
Paweł Wawrzynek <Pawel.Wawrzynek@equiniti.com>
2025-08-27 20:08:59 +0200

    Migrated footer to tailwindCSS

---
+++ client-app/src/components/Footer.tsx
@@ -0,0 +1,50 @@
+import packageJson from "../../package.json";
+import WasoftIcon from "@/resources/icons/wasoft.svg?react";
+import WawrzelniaIcon from "@/resources/icons/wawrzelnia.svg?react";
+import BitbucketIcon from "@/resources/icons/bitbucket.svg?react";
+import LinkedInIcon from "@/resources/icons/linkedin.svg?react";
+
+
+const Footer = () => {
+  return (
+    <footer className="relative w-full">
+      <div className="container">
+        <div className="flex flex-col md:flex-row items-center justify-between w-full">
+          <div className="flex items-center justify-center">
+            <WasoftIcon className="hidden md:block self-center h-16 mr-12" />
+            <a
+              href="https://www.linkedin.com/in/pawe%C5%82-wawrzynek-6b3433100/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="w-10 h-10 flex items-center justify-center m-4"
+            >
+              <LinkedInIcon className="transition-transform duration-200 fill-medium-light hover:fill-primary hover:scale-125" />
+            </a>
+            {/*TODO: Change Bitbucket to GitHub*/}
+            <a
+              href="https://bitbucket.org/Laurer/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="w-10 h-10 flex items-center justify-center m-4"
+            >
+              <BitbucketIcon className="transition-transform duration-200 fill-medium-light hover:fill-primary hover:scale-125" />
+            </a>
+            <a
+              href="https://www.wawrzelnia.pl/"
+              target="_blank"
+              rel="noreferrer noopener"
+              className="w-10 h-10 flex items-center justify-center m-4"
+            >
+              <WawrzelniaIcon className="transition-transform duration-200 stroke-medium-light hover:stroke-primary hover:scale-125" />
+            </a>
+          </div>
+          <p className="text-medium-light text-center leading-6">
+            v.{packageJson.version} | {new Date().getFullYear()} &copy;{' '}
+            <a href="http://www.wasoft.pl">WaSoft</a> All rights reserved
+          </p></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
--- client-app/src/components/home/Home.tsx
+++ client-app/src/components/home/Home.tsx
@@ -1,11 +1,11 @@
 import Header from "../header/Header";
-import Footer from "../footer/Footer";
 import Contact from "./Contact";
 import Education from "./Education";
 import Experience from "./Experience";
 import Hero from "./Hero";
 import Hobby from "./Hobby";
 import Skills from "./Skills";
+import Footer from "../Footer";
 
 const Home = () => {
   return (
