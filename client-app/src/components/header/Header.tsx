9a4fd48699eedae1bac3813a3872c2245d4b68c3
Paweł Wawrzynek <Pawel.Wawrzynek@equiniti.com>
2025-09-03 15:52:42 +0200

    Migrated MainNav to tailwind

--- client-app/src/components/header/Header.tsx
+++ client-app/src/components/header/Header.tsx
@@ -27,15 +27,13 @@ const Header = () => {
   }, []);
 
   return (
-    <div id="header">
-      <div className={`fixed top-0 w-full h-[90px] z-2 bg-dark bg-opacity-90 transition-all duration-1000 ${onTop ? "-translate-y-full" : "translate-y-0"}`}>
-        <div className="container h-full mx-auto flex items-center justify-between px-0 py-0">
-          <div className="h-full ml-4 flex items-center">
-            <WasoftLightIcon className="h-full w-auto" />
-          </div>
-          <MainNav />
-          <MobileNav />
+    <div className={`fixed top-0 w-full h-24 z-2 bg-dark bg-opacity-90 transition-all duration-1000 ${onTop ? "-translate-y-full" : "translate-y-0"}`}>
+      <div className="container h-full mx-auto flex items-center justify-between px-0 py-0">
+        <div className="h-full ml-4 flex items-center">
+          <WasoftLightIcon className="h-full w-auto" />
         </div>
+        <MainNav />
+        <MobileNav />
       </div>
     </div>
   );
--- client-app/src/components/header/MainNav.tsx
+++ client-app/src/components/header/MainNav.tsx
@@ -1,4 +1,3 @@
-import "./MainNav.scss";
 import LanguageSelector from "../controls/LanguageSelector";
 import { sections } from "./Header";
 import { Trans } from "react-i18next";
@@ -10,20 +9,21 @@ const MainNav = () => {
   };
 
   return (
-    /* TODO: Implement highlighting active section */
     <>
-      <div id="main-nav">
-        {sections.map((section) => {
-          return (
-            <button key={section} onClick={() => scroll(section)}>
-              <Trans i18nKey={`header.${section}`} />
-            </button>
-          );
-        })}
+      <div className="hidden md:flex flex-row items-center">
+        {sections.map((section) => (
+          <button
+            key={section}
+            onClick={() => scroll(section)}
+            className="mx-1 px-4 py-2 text-light font-teko font-medium text-[1.5rem] border-b-2 border-transparent hover:border-primary hover:text-primary transition-colors duration-200"
+          >
+            <Trans i18nKey={`header.${section}`} />
+          </button>
+        ))}
       </div>
       <LanguageSelector />
     </>
   );
-}
+};
 
 export default MainNav;
