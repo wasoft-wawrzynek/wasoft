0344662be8c0c014585e62d2c624102dd994e6a4
Paweł Wawrzynek <Pawel.Wawrzynek@equiniti.com>
2025-09-23 18:51:34 +0200

    Added animated dots in background

--- client-app/src/components/home/Hero.tsx
+++ client-app/src/components/home/Hero.tsx
@@ -1,6 +1,7 @@
 import { Trans, useTranslation } from "react-i18next";
 import Typed from "../controls/Typed";
 import WasoftIcon from "@/resources/icons/wasoft-light-full.svg?react";
+import AnimatedDots from "../controls/AnimatedDots";
 
 const Hero = () => {
   const { t } = useTranslation();
@@ -12,44 +13,46 @@ const Hero = () => {
   ];
 
   return (
-    <div id="hero" className="relative flex flex-col items-center justify-center h-[100vh] w-full">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 md:gap-[5em] w-full px-4 md:px-8">
-        {/* Logo Content */}
-        <div className="flex justify-center md:justify-end items-center">
-          <div className="relative mx-auto aspect-square w-[min(70vw,50vh)] max-w-[400px] max-h-[400px]">
-            {/* Profile bubble */}
-            <div className="rounded-full bg-dark border-4 border-primary overflow-hidden w-full h-full flex items-center justify-center">
-              <img
-                src="/img/profile.png"
-                alt="logo"
-              />
-            </div>
-            {/* Wasoft bubble */}
-            <div className="absolute w-1/2 h-1/2 p-[5%] bottom-[-15%] right-[-15%] rounded-full bg-dark border-4 border-primary flex items-center justify-center">
-              <WasoftIcon />
+    <AnimatedDots>
+      <div id="hero" className="relative flex flex-col items-center justify-center h-[100vh] w-full">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-20 md:gap-[5em] w-full px-4 md:px-8">
+          {/* Logo Content */}
+          <div className="flex justify-center md:justify-end items-center">
+            <div className="relative mx-auto aspect-square w-[min(70vw,50vh)] max-w-[400px] max-h-[400px]">
+              {/* Profile bubble */}
+              <div className="rounded-full bg-dark border-4 border-primary overflow-hidden w-full h-full flex items-center justify-center">
+                <img
+                  src="/img/profile.png"
+                  alt="logo"
+                />
+              </div>
+              {/* Wasoft bubble */}
+              <div className="absolute w-1/2 h-1/2 p-[5%] bottom-[-15%] right-[-15%] rounded-full bg-dark border-4 border-primary flex items-center justify-center">
+                <WasoftIcon />
+              </div>
             </div>
           </div>
+          {/* Info Content */}
+          <div className="flex flex-col text-center justify-center mx-0 md:mx-12 max-w-full min-w-[270px]">
+            <h3 className="mt-6 leading-[2.5rem]">
+              <Trans i18nKey="hero.introduction-start" />
+              <span className="text-primary">Paweł</span>
+              <Trans i18nKey="hero.introduction-end" />
+            </h3>
+            <h2 className="mt-4 font-bold text-primary">
+              <Typed
+                strings={words}
+                typeSpeed={40}
+                backSpeed={20}
+                backDelay={2000}
+                cursorChar="_"
+                loop
+              />
+            </h2>
+          </div>
         </div>
-        {/* Info Content */}
-        <div className="flex flex-col text-center justify-center mx-0 md:mx-12 max-w-full min-w-[270px]">
-          <h3 className="mt-6 leading-[2.5rem]">
-            <Trans i18nKey="hero.introduction-start" />
-            <span className="text-primary">Paweł</span>
-            <Trans i18nKey="hero.introduction-end" />
-          </h3>
-          <h2 className="mt-4 font-bold text-primary">
-            <Typed
-              strings={words}
-              typeSpeed={40}
-              backSpeed={20}
-              backDelay={2000}
-              cursorChar="_"
-              loop
-            />
-          </h2>
-        </div>
-      </div>
-    </div>
+      </div >
+    </AnimatedDots>
   );
 };
 
---
+++ client-app/src/constants/dots.ts
@@ -0,0 +1,5 @@
+export const DOT_COLOR = { r: 150, g: 253, b: 0 };
+export const DOT_RADIUS = 3;
+export const LINE_WIDTH = 2;
+export const LINE_MIN_OPACITY = 0.1;
+export const LINE_MAX_OPACITY = 0.8;
