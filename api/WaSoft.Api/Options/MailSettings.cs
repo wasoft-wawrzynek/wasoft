633d2641cdc9558dcdb82ee4a4e4fee969c249d9
Laurer <pawel.wawrzynek@gmail.com>
2025-10-11 16:23:15 +0200

    Migrated Azure Functions project to .NET 9

---
+++ api/WaSoft.Api/Options/MailSettings.cs
@@ -0,0 +1,11 @@
+﻿namespace WaSoft.Api.Options;
+
+public class MailSettings
+{
+    public string ApiKey { get; set; }
+    public string TemplateId { get; set; }
+    public string FromEmail { get; set; }
+    public string FromName { get; set; }
+    public string ToEmail { get; set; }
+    public string ToName { get; set; }
+}
---
+++ api/WaSoft.Api/Services/MailSender.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using SendGrid;
+using SendGrid.Helpers.Mail;
+using WaSoft.Api.Dto;
+using MailSettings = WaSoft.Api.Options.MailSettings;
+
+namespace WaSoft.Api.Services;
+
+public interface IMailSender
+{
+    Task<bool> SendAsync(ContactFormData contactForm);
+}
+
+public class MailSender(ILogger<MailSender> logger, IOptions<MailSettings> mailConfig) : IMailSender
+{
+    public async Task<bool> SendAsync(ContactFormData contactForm)
+    {
+        try
+        {
+            var client = new SendGridClient(mailConfig.Value.ApiKey);
+            var message = CreateMessage(contactForm);
+
+            logger.LogInformation("Message successfully created, requesting SendGrid to process it...");
+            var response = await client.SendEmailAsync(message);
+            if (!response.IsSuccessStatusCode)
+            {
+                var content = await response.Body.ReadAsStringAsync();
+                logger.LogError($"SendGrid responded with a non-success status ({response.StatusCode}). Content:\n{content}");
+                return false;
+            }
+
+            return true;
+        }
+        catch (Exception ex)
+        {
+            logger.LogError($"Something went wrong, when preparing contact message: {ex.Message}\n\nContact form data:\n{contactForm}");
+            return false;
+        }
+    }
+
+    private SendGridMessage CreateMessage(ContactFormData contactForm)
+    {
+        contactForm.Date = DateTime.UtcNow.ToString("dd'/'MM'/'yyyy HH:mm");
+        contactForm.Message = contactForm.Message.ReplaceLineEndings("<br>");
+
+        var from = new EmailAddress(mailConfig.Value.FromEmail, mailConfig.Value.FromName);
+        var to = new EmailAddress(mailConfig.Value.ToEmail, mailConfig.Value.ToName);
+
+        var mail = MailHelper.CreateSingleTemplateEmail(from, to, mailConfig.Value.TemplateId, contactForm);
+        mail.ReplyTo = new EmailAddress(contactForm.Email);
+        return mail;
+    }
+}
