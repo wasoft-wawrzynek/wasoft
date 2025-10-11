d07aaf380d2f487d9f6f65ceef487956f629f69a
Laurer <pawel.wawrzynek@gmail.com>
2025-10-11 22:43:06 +0200

    Post-PR updates

--- client-app/src/api/client.ts
+++ client-app/src/api/client.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 import IContactFormData from "./models/IContactFormData";
+import appConfig from "../config/appConfig";
 
-axios.defaults.baseURL = process.env.REACT_APP_API_URL;
+axios.defaults.baseURL = appConfig.API_URL;
 
 const Contact = {
   sendContactForm: async (form: IContactFormData): Promise<boolean> => {
